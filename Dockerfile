f56ee55ff637a3e2f962bd2d1ccd071b42c46730
Pedro Sanders <sanderspedro@gmail.com>
2020-07-25 18:05:28 -0400

    Fixed docker builder

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ LABEL maintainer="Pedro Sanders <fonosterteam@fonoster.com>"
 COPY . /uploaderhelper
 WORKDIR /uploaderhelper
 
-RUN apk add --update nodejs=10.19.0-r0 npm=10.19.0-r0 ;\
+RUN apk add --update nodejs=10.19.0-r0 npm=10.19.0-r0 \
+  python=2.7.18-r0 make=4.2.1-r2 g++=8.3.0-r0 ; \
   npm -g install . ; \
+  npm rebuild ; \
   rm -rf /var/cache/apk/* /tmp/* /var/tmp/*
 
 CMD ["/bin/sh", "-c", "run-uploaderhelper"]
--- uploaderhelper.js
+++ uploaderhelper.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
-const Storage = require('@fonos/storage')
+const Storage = require('@fonos/storage').default
 const chokidar = require('chokidar')
 const fs = require('fs')
-const METADATA = { 'Content-Type': 'audio/x-wav' }
+const METADATA = process.env.METADATA || { 'Content-Type': 'audio/x-wav' }
+const BUCKET = process.env.BUCKET || 'default'
+
+console.log(`Uploader helper. Watching /data volume`)
 
 chokidar.watch('/data', { ignoreInitial: true }).on('add', pathToFile => {
-  console.log(`File ${path} has been added. Pushing to files server`)
+  console.log(`File ${pathToFile} has been added. Pushing to bucket '${BUCKET}'`)
   const storage = new Storage()
-  storage.uploadObjectSync({
-    filename: pathToFile,
-    bucket: process.env.BUCKET || 'default',
-    metadata: process.env.METADATA || METADATA
-  })
+
+  try {
+    storage.uploadObjectSync({
+      filename: pathToFile,
+      bucket: BUCKET,
+      metadata: METADATA
+    })
+  } catch(e) {
+    console.log('Unable to upload file =>', e)
+  }
 
   if(process.env.REMOVE_AFTER_UPLOAD &&
     process.env.REMOVE_AFTER_UPLOAD.toLocaleLowerCase === "true") {
-    fs.unlink(path)
+    fs.unlink(pathToFile)
   }
 })
