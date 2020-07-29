89aaf7c617b4c2d2995aeb2383e6ebf905e63d80
Pedro Sanders <sanderspedro@gmail.com>
2020-07-29 08:47:24 -0400

    Minor fix

--- uploaderhelper.js
+++ uploaderhelper.js
@@ -3,6 +3,9 @@ const Storage = require('@fonos/storage').default
 const EventsRecvr = require('@fonos/events').EventsRecvr
 const logger = require('@fonos/logger').default
 const fs = require('fs')
+const { join } = require('path')
+
+const BASE_DIR = '/data/recordings'
 const METADATA = process.env.METADATA || { 'Content-Type': 'audio/x-wav' }
 const BUCKET = process.env.BUCKET || 'default'
 
@@ -19,7 +22,7 @@ er.watchEvents(content => {
   const event = JSON.parse(content.toString())
 
   if (event.name === 'RECORDING_CREATED') {
-    const pathToFile = event.data.pathToFile
+    const pathToFile = join(BASE_DIR, event.data.filename)
     logger.log('debug', `File ${pathToFile} has been added. Pushing to bucket '${BUCKET}'`)
     const storage = new Storage()
 
